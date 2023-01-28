a08a6eded9d06bd6136619d77a49c912864a944b
sharonabrahamshaji <s222555241@deakin.edu.au>
2023-01-28 15:26:35 +1100

    Commit Week 6

--- Controller/projectController.js
+++ Controller/projectController.js
@@ -1,48 +1,44 @@
-
 let projectModel = require("../Models/project")
-//let client = require("../dbConnect");
-// let projectCollection;
 
+// let client = require("../dbConnect")
+// let projectCollection;/\
 
 // setTimeout(() => {
 //     projectCollection = client.mongoClient.db().collection("Projects");
 // }, 2000)
 
-// const insertProjects = (project,callback) => {
-//         projectCollection.insert(project,callback);
-//     }
+// const insertProjects =(project,callback) =>{
+//     projectCollection.insert(project,callback);
+// }
+// //get project
+// const getProjects = (callback) =>{
+//     projectCollection.find({}).toArray(callback)
+// }
 
-//      //  get project...
-// const getProjects = (callback) => {
-//         projectCollection.find({}).toArray(callback);
-//     }
+//Create project
+const createProject = (req, res) => {
+    console.log("New Project added", req.body)
+    var newProjects = req.body;
+    projectModel.insertProjects(newProjects,(err,result) =>{
+        if(req){
+            res.json({statusCode: 400,message: err})
+        }
+        else{
+            res.json({statusCode: 200, message:"Project added sucessfully", data: result})
+        }
+    })
+}
 
+//retrieve project
+const retrieveProject = (req, res) => {
+    projectModel.getProjects((err,result) => {
+        if(err){
+            res.json({statusCode: 400, message: err})
+        }
+        else{
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+}
 
-    //create project
-    const createProject = (req,res) => {
-        console.log("New Project added", req.body)
-            var newProject = req.body;
-            projectModel.insertProjects(newProject,(err,result) => {
-                if(err) {
-                    res.json({statusCode: 400, message: err})
-                }
-                else {
-                    res.json({statusCode: 200, message:"Project Successfully added", data: result})
-                }
-            })
-    }
-    //retrieve project...
-    const retrieveProjects = (req,res) => {
-
-
-        projectModel.getProjects((err,result) => {
-                if(err) {
-                    res.json({statusCode: 400, message: err})
-                }
-                else {
-                    res.json({statusCode: 200, message:"Success", data: result})
-                }
-            })
-    }
-
-    module.exports = (retrieveProjects, createProject)
+module.exports = {retrieveProject, createProject}
--- Routes/projectRoutes.js
+++ Routes/projectRoutes.js
@@ -1,59 +1,47 @@
-var express = require("express");
-const projectController = require("../Controller/projectController");
+var express = require("express")
 var router = express.Router();
-
-// let client = require("../dbConnect");
-// let projectCollection;
-let controller = require("../Controller")
+// let client = require("../dbConnect")
+let projectController = require("../Controller/projectController")
 
 // setTimeout(() => {
-//     projectCollection = client.mongoClient.db().ollection("Projects");
+//     projectCollection = client.mongoClient.db().collection("Projects");
 // }, 2000)
 
-// const insertProjects = (project,callback) => {
-//         projectCollection.insert(project,callback);
-//     }
+// const insertProjects =(project,callback) =>{
+//     projectCollection.insert(project,callback);
+// }
 
-// // get project...
-// const getProjects = (callback) => {
-//         projectCollection.find({}).toArray(callback);
-//     }
+//post api
+router.post('/',(req,res) => {
+    projectController.createProject(req,res)
+    // console.log("New Project added", req.body)
+    // var newProjects = req.body;
+    // insertProjects(newProjects,(err,result) =>{
+    //     if(ree){
+    //         res.json({statusCode: 400,message: err})
+    //     }
+    //     else{
+    //         res.json({statusCode: 200, message:"Project added sucessfully", data: result})
+    //     }
+    // })
+})
 
+
+//get project
+// const getProjects = (callback) =>{
+//     projectCollection.find({}).toArray(callback)
+// }
 
 router.get('/',(req,res) => {
-    controller.projectController.createProjects(req, res)
-    //     getProjects((err,result) => {
-    //         if(err) {
-    //             res.json({statusCode: 400, message: err})
-    //         }
-    //         else {
-    //             res.json({statusCode: 200, message:"Success", data: result})
-    //         }
-    //     })
-    })
+    projectController.retrieveProject(req, res)
+    // getProjects((err,result) => {
+    //     if(err){
+    //         res.json({statusCode: 400, message: err})
+    //     }
+    //     else{
+    //         res.json({statusCode: 200, message:"Success", data: result})
+    //     }
+    // })
+})
 
-//     //  get project...
-// const getProjects = (callback) => {
-//          projectCollection.find({}).toArray(callback);
-//      }
-
-
-    //post api
-    router.post('/api/projects',(req,res) => {
-        controller.projectController.retrieveProjects(req, res)
-
-        //     console.log("New Project added", req.body)
-        //     var newProject = req.body;
-        //     insertProjects(newProject,(err,result) => {
-        //         if(err) {
-        //             res.json({statusCode: 400, message: err})
-        //         }
-        //         else {
-        //             res.json({statusCode: 200, message:"Project Successfully added", data: result})
-        //         }
-        //     })
-        })
-        
-
-        module.exports = router;
-        
+module.exports =router;
